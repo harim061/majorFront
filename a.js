96706c9b63fc119c966b0d3006e7b9c332d64b52
harim061 <doris5093@duksung.ac.kr>
2022-08-11 01:38:16 +0900

    배경 글자 효과 수정

--- a.js
+++ a.js
@@ -27,6 +27,45 @@ $(window).on("wheel", function(e){
     $html.animate({scrollTop : posTop});
 });
 
+/*별*/
+
+const $sky = document.querySelector(".sky");
+
+// 브라우저 창 크기에 따른 별 생성
+window.onresize = () => {
+makeStars();
+}
+
+const makeStars = () => {
+// 브라우저 가장 큰 크기
+const maxSize = Math.max(window.innerWidth, window.innerHeight)
+
+// 랜덤한 X 위치 값
+const getRandomX = () => Math.random() * maxSize;
+
+// 랜덤한 Y 위치 값
+const getRandomY = () => Math.random() * maxSize;
+
+// 랜덤한 크기 (circle는 반지름이 크기)
+const randomRadius = () =>  Math.random() * 0.7 + 0.6;
+
+// 임의의 값
+const _size = Math.floor(maxSize / 2);
+
+const htmlDummy = new Array(_size).fill().map((_, i) => {
+    return  `<circle class='star'
+        cx=${getRandomX()}
+        cy=${getRandomY()}
+        r=${randomRadius()}
+        className="star" />`
+}).join('');
+
+$sky.innerHTML = htmlDummy;
+}
+
+window.onload = () => {
+makeStars();
+/* */
 jQuery(document).ready(function($){
 
     var bArray = [];
@@ -176,5 +215,4 @@ const closeBtn4 = modal4.querySelector("#closeetc")
 closeBtn4.addEventListener("click", e => {
     modal4.style.display = "none";
     etcmodal.style.display=="none";
-})
-
+})}
