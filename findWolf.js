b30275cb5fc2f9e56aa05ebfcae4f7e514828071
harim061 <doris5093@duksung.ac.kr>
2022-08-15 19:35:31 +0900

    늑때 숨김, 플로우 로고 스쿨라이프에 추가, 글씨체 통일 몇개 디자인 수정

---
+++ findWolf.js
@@ -0,0 +1,31 @@
+function find(){
+    document.querySelector(".findWolf1").style.display = "block";
+}
+function out(){
+    document.querySelector(".findWolf1").style.display = "none";
+}
+
+function find2(){
+    document.querySelector(".findWolf2").style.display = "block";
+}
+function out2(){
+    document.querySelector(".findWolf2").style.display = "none";
+}
+
+function flow(){
+    document.querySelector
+    (".flowimg").style.display = "block";
+}
+
+function flowout(){
+    document.querySelector(".flowimg").style.display = "none";
+}
+
+function cn(){
+    document.querySelector
+    (".cnimg").style.display = "block";
+}
+
+function cnout(){
+    document.querySelector(".cnimg").style.display = "none";
+}
